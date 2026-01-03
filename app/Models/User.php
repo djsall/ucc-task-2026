03a89033f8bfeff2083398a6d5e9076dde02ec9b
Levente Proksa <proksalevente@gmail.com>
2026-01-03 21:45:26 +0100

    fix: linebreaks

--- app/Models/User.php
+++ app/Models/User.php
@@ -15,15 +15,11 @@ class User extends Authenticatable
     use HasApiTokens, HasFactory, Notifiable;
 
     protected $fillable = [
-        'name',
-        'email',
-        'password',
-        'role',
+        'name', 'email', 'password', 'role',
     ];
 
     protected $hidden = [
-        'password',
-        'remember_token',
+        'password', 'remember_token',
     ];
 
     protected function casts(): array
